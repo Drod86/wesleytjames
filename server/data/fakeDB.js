e336f8151bae675603a040078b6136460c74417c
Daniel Rodriguez <daniel.r.rodriguez86@gmail.com>
2023-09-26 14:35:18 -0500

    Build file structure for React app with Express API

---
+++ server/data/fakeDB.js
@@ -0,0 +1,32 @@
+// Admin Databases
+export const members = {
+  'roddan123': {
+    id: 'roddan123',
+    firstName: 'Daniel',
+    lastName: 'Rodriguez',
+    email: 'daniel.r.rodriguez86@gmail.com',
+    appointments: ['2023-09-27T11:00', '2023-09-29T11:00'],
+    credits: 0,
+  },
+  'smijoh123': {
+    id: 'smijoh123',
+    firstName: 'John',
+    lastName: 'Smith',
+    email: 'john.smith@gmail.com',
+    appointments: [],
+    credits: 1,
+  },
+  'doejan123': {
+    id: 'doejan123',
+    firstName: 'Jane',
+    lastName: 'Doe',
+    email: 'jane.doe@gmail.com',
+    appointments: [],
+    credits: 2,
+  }
+}
+
+const AllAppointments = [
+  {'2023-09-27T11:00':'roddan123'},
+  {'2023-09-29T11:00':'roddan123'}
+]
--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+import React, {useEffect, useState} from "react";
+
 export function App() {
   return <h1>Let's start building wesleytjames.com</h1>;
 }
